0d78057036dcb16719ed6521fa98f1640de6f6ee
Prashanth G K <prashkammar17@gmail.com>
2020-08-02 10:48:01 +0530

    reset_password

--- blog/config.py
+++ blog/config.py
@@ -5,7 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     DEBUG = False
     TESTING = False
-    #CSRF_ENABLED = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
