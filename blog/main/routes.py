e967f6a283aea053133d72d666699ba5cbd86e23
Prashanth G K <prashkammar17@gmail.com>
2020-08-27 13:16:51 +0530

    staticweb

--- blog/main/routes.py
+++ blog/main/routes.py
@@ -1,18 +1,21 @@
 from flask import Blueprint, render_template, request
 from blog.models import Post
 
-main = Blueprint('main', __name__)
+main = Blueprint("main", __name__)
 
 
-@main.route('/')
-@main.route('/home')
+@main.route("/")
+def index():
+    return render_template("index.html")
+
+
+@main.route("/home")
 def home():
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(
-        Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template("home.html", posts=posts)
 
 
 @main.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
