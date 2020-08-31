021922e53ddb9a709446963abd147d1783507d54
Prashanth G K <prashkammar17@gmail.com>
2020-08-31 15:19:47 +0530

    register page

---
+++ blog/main/forms.py
@@ -0,0 +1,67 @@
+from flask_wtf import Form
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Length, Email
+
+
+def strip_filter(x):
+    return x.strip() if x else None
+
+
+class MemberForm(Form):
+    firstname = StringField(
+        "First Name",
+        validators=[DataRequired(), Length(min=2, max=30)],
+        filters=[strip_filter]
+    )
+    lastname = StringField(
+        "Last Name",
+        validators=[DataRequired(), Length(min=2, max=30)],
+        filters=[strip_filter]
+    )
+    email = StringField(
+        "Email", validators=[DataRequired(), Email()], filters=[strip_filter]
+    )
+    phone = StringField(
+        "Phone",
+        validators=[
+            DataRequired(),
+            Length(min=10, max=13, message="Enter your valid mobile number"),
+        ],
+        filters=[strip_filter],
+        render_kw={
+            "type": "number",
+            "autocomplete": "off",
+            "onfocus": "this.removeAttribute('readonly');",
+        }
+    )
+    usn = StringField(
+        "USN",
+        validators=[DataRequired(), Length(min=2, max=30)],
+        filters=[strip_filter]
+    )
+    branch = SelectField(
+        "Branch",
+        choices=[
+            ("ECE", "Electronics & Communication Engineering"),
+            ("CSE", "Computer Science Engineering"),
+            ("EEE", "Electrical & Electronics Engineering"),
+            ("ISE", "Information Science Engineering"),
+            ("MECH", "Mechanical Engineering"),
+            ("Civil", "Civil Engineering")
+        ]
+    )
+    semester = SelectField(
+        "semester",
+        choices=[
+            ("1", "1"),
+            ("2", "2"),
+            ("3", "3"),
+            ("4", "4"),
+            ("5", "5"),
+            ("6", "6"),
+            ("7", "7"),
+            ("8", "8")
+        ]
+    )
+    submit = SubmitField("Become a member")
+
--- blog/main/routes.py
+++ blog/main/routes.py
@@ -1,12 +1,46 @@
 from flask import Blueprint, render_template, request
+from blog.main.forms import MemberForm
 from blog.models import Post
+import json
+import requests
 
 main = Blueprint("main", __name__)
 
 
+def send_to_google(values):
+    url = "https://script.google.com/macros/s/AKfycbza4Lx4a1AO9Ea48C57OoqTGYq2Asqf0ipYG0R5-N8cLj2Fwpc/exec"
+    params = {"variables": values}
+    requests.post(url, params=params)
+
+
 @main.route("/")
 def index():
-    return render_template("index.html")
+    form = MemberForm()
+    return render_template("index.html", form=form)
+
+
+@main.route("/member", methods=["GET", "POST"])
+def member():
+    form = MemberForm()
+    if request.method == "POST" and form.validate_on_submit():
+        values = {
+            "firstname": form.firstname.data,
+            "lastname": form.lastname.data,
+            "email": form.email.data,
+            "phone": form.phone.data,
+            "usn": form.usn.data,
+            "branch": form.branch.data,
+            "semester": form.semester.data,
+        }
+        send_to_google(values)
+        return render_template("member.html", form=form)
+        # flash(
+        #     " Dear {}, your registration was successful!".format(
+        #         form.firstname.data + form.lastname.data
+        #     ),
+        #     "success",
+        # )
+    return render_template("member.html", form=form)
 
 
 @main.route("/home")
